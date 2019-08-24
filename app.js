196b114406e595b2097699bff95270934bbfe919
connander <connorpstark@gmail.com>
2019-08-24 15:22:00 -0500

    got the api to work, needs to work with the rest of the page however

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-var queryURL= "https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=58863c39&app_key=442ed61f8f641823d49aa8c6ea14c582";
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/search?offset=0&number=10&maxCalories=5000&minProtein=0&maxProtein=100&minFat=0&maxFat=100&minCarbs=0&maxCarbs=100&minCalories=0&query=snickers",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key": "44b03f43d8msh496c03f2d1c35bdp148563jsnf3e44a02f2ee"
+	}
+}
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
