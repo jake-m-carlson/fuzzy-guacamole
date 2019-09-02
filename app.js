a7896bdf2e9047aaad7554442781ae860ed59a69
connander <connorpstark@gmail.com>
2019-09-02 15:58:29 -0500

    finished work up with spoonacular api

--- app.js
+++ app.js
@@ -14,25 +14,36 @@ var settings = {
 	}
 }
 
-/* var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-		"x-rapidapi-key": "44b03f43d8msh496c03f2d1c35bdp148563jsnf3e44a02f2ee"
-	}
-} 
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-*/
-
 $.ajax(settings).done(function (response) {
 	var ingredientNumber = response.products;
-	console.log(ingredientNumber);
+	var ingredientId= ingredientNumber[0].id;
 	console.log(response);
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/" + ingredientId,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+			"x-rapidapi-key": "44b03f43d8msh496c03f2d1c35bdp148563jsnf3e44a02f2ee"
+		}
+	} 
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+		var nutritional = response.nutrition;
+		console.log(nutritional);
+		var calorie = nutritional.calories;
+		$("#calorie").text(calorie);
+		var fat = nutritional.fat;
+		$("#fat").text(fat);
+		var protein = nutritional.protein;
+		$("#protein").text(protein);
+		var carbs = nutritional.carbs;
+		$("#carbs").text(carbs);
+	});
 });
+
+
 });
