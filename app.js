e7c5a57cf2a7aa172435cd0ecb8eac00e70cf5c3
connander <connorpstark@gmail.com>
2019-09-02 15:35:10 -0500

    part of the api is working now

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-$("#foodItembutton").click(function(){
+$("#foodItemButton").click(function(event){
+	event.preventDefault();
 	var ingredientSearch = $("#foodInput").val();
+	console.log(ingredientSearch);
 
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/search?query=" + ingredientSearch,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/search?number=1&query=" + ingredientSearch,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
@@ -12,7 +14,25 @@ var settings = {
 	}
 }
 
+/* var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key": "44b03f43d8msh496c03f2d1c35bdp148563jsnf3e44a02f2ee"
+	}
+} 
+
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
-});	
+*/
+
+$.ajax(settings).done(function (response) {
+	var ingredientNumber = response.products;
+	console.log(ingredientNumber);
+	console.log(response);
+});
+});
